0baaf576e342a35b896e1fe307f74167f4731d00
jonathansbsp <102991806+jonathansbsp@users.noreply.github.com>
2022-06-24 18:43:56 -0300

    Entrega Solution Sprint - FINAL

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Laboratório Pipeline DevOps v4 - Solution Sprint Concluido By:Jonathan")
+        self.assertEqual(self.result.data.decode('utf-8'), "Laboratório Pipeline DevOps v4 - Solution Sprint-FINAL - Concluido By:Jonathan")
